c55f9477331bfc64208757a49531412d86ee4778
Soton Ahmed <soton371@gmail.com>
2024-08-09 11:01:11 +0600

    bloc fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:m360_career/views/views.dart';
 import '../configs/configs.dart';
+import 'blocs/blocs.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,19 +15,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(393, 852),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      ensureScreenSize: true,
-      builder: (context, child) {
-        return MaterialApp(
-          title: 'M360 Career',
-          theme: kAppTheme(context),
-          home: child,
-        );
-      },
-      child: const LoginScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(393, 852),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        ensureScreenSize: true,
+        builder: (context, child) {
+          return MaterialApp(
+            title: 'M360 Career',
+            theme: kAppTheme(context),
+            home: child,
+          );
+        },
+        child: const LoginScreen(),
+      ),
     );
   }
 }
