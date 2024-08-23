ff0d9bce19e03ce113d30cc3f48fc369bdcf6343
Soton Ahmed <soton371@gmail.com>
2024-08-23 09:40:55 +0600

    hash string

---
+++ lib/utilities/hash_string.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+
+String hashString(String password) {
+  final bytes = utf8.encode(password);
+  final digest = sha256.convert(bytes);
+  return digest.toString();
+}
--- lib/utilities/utilities.dart
+++ lib/utilities/utilities.dart
@@ -1,2 +1,3 @@
 export 'app_parse_json.dart';
 export 'app_validations.dart';
+export 'hash_string.dart';
